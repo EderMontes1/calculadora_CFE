c5a5c9b5de51029aaa77d2eaddd2c96a78c1480c
Ing. Eder Montes <amontesr1@hotmail.com>
2024-04-09 17:34:19 -0600

    Se mejora interfaz (Agrega Ingresar Consumo Inicial y Consumo Final) Se agrega IVA en el calculo de PagoTotal

--- calculadora_CFE.py
+++ calculadora_CFE.py
@@ -6,6 +6,7 @@ from tkinter import ttk
 from tkinter import Menu
 import sys
 
+
 #Se agrega clase 
 class crearVentana(tk.Tk):
     def __init__(self): #Se declara metodo init
@@ -17,6 +18,7 @@ class crearVentana(tk.Tk):
         self.tarifaBasico = 1.023 * 75
         self.tarifaIntermedio = 1.247 * 65
         self.tarifaExcedente = 3.646
+        self.iva_vigente = 0.16
        
         
 
@@ -32,33 +34,38 @@ class crearVentana(tk.Tk):
 
     def _crearComponentes(self):
         titulo = ttk.Label(self, text="Calculadora Consumo CFE" , font=16).place(x=110, y=30)
-        etiqueta = ttk.Label(self, text="Ingresa tu consumo mensual:").place(x=100, y=80)
-        etiqueta2 = ttk.Label(self, text="Tu consumo bimestral es de: ").place(x=100, y=120)
-        etiqueta3 = ttk.Label(self, text="Tu total a pagar aproximado es de: ").place(x=100, y=150)
+        etiqueta_Inicial = ttk.Label(self, text="Ingresa tu consumo inicial:").place(x=100, y=80)
+        etiqueta_Final = ttk.Label(self, text="Ingresa tu consumo final:").place(x=100, y=120)
+        etiqueta2 = ttk.Label(self, text="Tu consumo bimestral es de: ").place(x=100, y=170)
+        etiqueta3 = ttk.Label(self, text="Tu total a pagar aproximado es de: ").place(x=100, y=200)
 
         #Boton Entry (Se debe separar el .place en 2 lineas o dara como resultado un error)
-        self.entrada = ttk.Entry(self, width=4, justify=tk.CENTER)
-        self.entrada.place(x=280, y=80)
+        self.entrada_inicial = ttk.Entry(self, width=4, justify=tk.CENTER)
+        self.entrada_inicial.place(x=280, y=80)
+
+        self.entrada_final = ttk.Entry(self, width=4, justify=tk.CENTER)
+        self.entrada_final.place(x=280, y=120)
         
         #Boton Calcular
         boton_calcular = ttk.Button(self, text="Calcular consumo", command=self.enviar).place(x=150, y=250)
         
-        etiqueta_consumobimestral = ttk.Label(self, textvariable=self.consumoBimestral).place(x=270, y=120)
-        etiqueta_pagototal = ttk.Label(self, textvariable=self.pagoTotal).place(x=300, y=150)
+        etiqueta_consumobimestral = ttk.Label(self, textvariable=self.consumoBimestral).place(x=285, y=170)
+        etiqueta_pagototal = ttk.Label(self, textvariable=self.pagoTotal).place(x=300, y=200)
         
 
     def enviar(self):
-        consumoMensual = int(self.entrada.get()) * 2
+        consumoMensual = int(self.entrada_final.get()) - int((self.entrada_inicial.get()))
         self.consumoBimestral.set(consumoMensual)
         
-        if self.consumoBimestral.get() >= 140:
+        if self.consumoBimestral.get() >= 140: #Tarifa  Mayor a 140 KwH
             self.pago = float((self.tarifaBasico + self.tarifaIntermedio) + (self.tarifaExcedente * (self.consumoBimestral.get() - 140)))
             
-        elif self.consumoBimestral.get() > 0 and self.consumoBimestral.get() < 76:
+        elif self.consumoBimestral.get() > 0 and self.consumoBimestral.get() < 76: #Tarifa > 0 y < 76 KwH
             self.pago = float((self.consumoBimestral.get() * 1.023))
-        elif self.consumoBimestral.get() > 75 and self.consumoBimestral.get() < 140:
-            self.pago = float((self.tarifaBasico) + ((self.consumoBimestral.get() - 75) * 1.2470))
-        return self.pagoTotal.set(self.pago)
+        elif self.consumoBimestral.get() > 75 and self.consumoBimestral.get() < 140: #Tarifa >75 y < 140 KwH
+            self.pago = float((self.tarifaBasico) + (self.consumoBimestral.get() - 75) * 1.24)
+       
+        return self.pagoTotal.set(round(float(self.pago * self.iva_vigente + (self.pago)),2)) #(Pago * IVA) + Pago Y se asigna el valor a la variable de la etiqueta pago total.
     
     def crear_menu(self):
         menu_principal = Menu(self)
